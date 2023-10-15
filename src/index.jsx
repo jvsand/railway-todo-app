22c78899ee243153f8a79fd640bf09e032d98682
jvsand <jave.shunya.623@gmail.com>
2023-10-15 18:29:51 +0900

    修正

---
+++ src/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { Provider } from 'react-redux'; // 必要に応じて追加
+import { CookiesProvider } from 'react-cookie'; // 必要に応じて追加
+import { store } from './store';
+import App from './App'; // Appコンポーネントのファイルパスに合わせて修正
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      {' '}
+      {/* Reduxストアを提供 */}
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
+    </Provider>
+  </React.StrictMode>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import {
   BrowserRouter, Route, Routes, Navigate,
 } from 'react-router-dom';
